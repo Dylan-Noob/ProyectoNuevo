72be2005197e5354d3181409bd0846e456b4fa93
Dylan-Noob <drojaso@iesch.org>
2025-11-21 20:48:03 +0100

    PrimerCommit21Nov

---
+++ src/App.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class App {
+    void main() {
+
+        .println(String.format("Write console tests"));
+
+        Scanner teclado = new Scanner(System.in);
+
+        for (; true; ) {
+            IO.println("Choose an option:\n" +
+                    "  1. Square\n" +
+                    "  2. Title\n" +
+                    "  0. Quit\n");
+
+            int numero = teclado.nextInt();
+
+            if (numero == 1) {
+                System.out.printf("Lado del cuadrado: ");
+                int lado = teclado.nextInt();
+                Figures.writeSquare(lado);
+            } else if (numero == 2) {
+                System.out.printf("Titulo: ");
+                String titulo = teclado.nextLine();
+                titulo = teclado.nextLine();
+                Titles.writeTitle(titulo);
+            } else if (numero == 0) {
+                Console.close();
+            }
+        }
+    }
+}
